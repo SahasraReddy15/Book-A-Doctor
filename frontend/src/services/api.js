6712e46d1cd7f3854a95d19065f2f8398b34f2f0
Sahasra <sahasrareddyy15@gmail.com>
2026-06-30 23:41:50 +0530

    Connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://book-a-doctor-vy0a.onrender.com/api"
 });
 
 export default API;
